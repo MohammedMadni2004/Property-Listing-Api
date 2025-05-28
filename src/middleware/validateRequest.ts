60a2906895eaec6ed3a0504a63103720e1fe6ed7
Madni <mohammedmadni09@gmail.com>
2025-05-28 20:22:51 +0530

    feat: enhance validateRequest middleware to include user authentication and token verification

--- src/middleware/validateRequest.ts
+++ src/middleware/validateRequest.ts
@@ -1,11 +1,16 @@
 import {Request, Response, NextFunction} from 'express';
 import {verifyToken} from '../utils/tokenUtils';
+import { UserModel } from '../models/usereModel';
+import { CustomRequest } from '../types/Request';
 
-const validateRequest = (req: Request, res: Response, next: NextFunction) => {
+async function  validateRequest  (req: CustomRequest, res: Response, next: NextFunction)  {
     const token = req.headers.authorization?.split(' ')[1];
     if (!token) return res.status(401).json({error: 'Unauthorized'});
     try {
-        verifyToken(token);
+        const decodedId =verifyToken(token);
+        const user= await UserModel.findOne({_id: decodedId});
+        if (!user) return res.status(401).json({error: 'Unauthorized'});
+        req.user = user; 
         next();
     } catch (error) {
         res.status(401).json({error: 'Invalid token'});
