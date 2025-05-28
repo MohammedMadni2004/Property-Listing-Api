1247b7d151f8eb9edc8a001de446241fd9da2593
Madni <mohammedmadni09@gmail.com>
2025-05-28 19:20:56 +0530

    feat: add body-parser dependency and update index.ts to use it for request parsing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import {connectDatabase} from './config';
 import dotenv from 'dotenv';
 import express from 'express';
+import bodyParser from 'body-parser';
+import authRoutes from './routes/authRoutes';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
