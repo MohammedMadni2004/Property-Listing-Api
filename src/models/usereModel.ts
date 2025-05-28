26454dc4faf21479b64c2a3de5981c23ced84cf6
Madni <mohammedmadni09@gmail.com>
2025-05-28 19:22:28 +0530

    feat: define IUser and IUserDocument interfaces for user model

--- src/models/usereModel.ts
+++ src/models/usereModel.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import { IUserDocument } from "../types/user";
 
-const userSChema = new mongoose.Schema({
+const userSChema = new mongoose.Schema<IUserDocument>({
     name: { type: String, required: true },
     email : { type: String, required: true, unique: true },
     password: { type: String, required: true },
 });
 
-export const UserModel = mongoose.model('User', userSChema);
+export const UserModel = mongoose.model<IUserDocument>('User', userSChema);
