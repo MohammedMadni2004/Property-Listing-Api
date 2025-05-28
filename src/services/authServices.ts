fc2b422cf6703947054821c1320168c82505fb92
Madni <mohammedmadni09@gmail.com>
2025-05-28 19:27:29 +0530

    feat: add user creation and token utility functions required for user authentication

---
+++ src/services/authServices.ts
@@ -0,0 +1,27 @@
+import { UserModel } from '../models/usereModel';
+import bcrypt from 'bcrypt';
+
+type CreateUserInput = { 
+  name: string;
+  email: string;
+  password: string;
+}
+
+export async function createUser({ name, email, password }: CreateUserInput) {
+  const hashedPassword = await bcrypt.hash(password, 10);
+   try{
+  const newUser = UserModel.create({
+    name,
+    email,
+    password: hashedPassword,
+    });
+    return newUser;
+ } catch (error) {
+    console.error('Error creating user:', error);
+    throw new Error('User creation failed');
+    }
+}
+
+export async function findUserByEmail(email: string) {
+  return await UserModel.findOne({ email });
+}
