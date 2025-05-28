c9cc634ee5b822036ea7573494bc52aa1c5b8b74
Madni <mohammedmadni09@gmail.com>
2025-05-28 18:19:24 +0530

    feat: implement server setup with express and connect to database

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 
 function connectDatabase() {
-    console.log(process.env.DB_URL); 
     mongoose.connect(process.env.DB_URL || '').then(
         () => { console.log("connected succesfully") }
     ).catch(err => {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
 import {connectDatabase} from './config';
 import dotenv from 'dotenv';
+import express from 'express';
 dotenv.config();
-console.log('Hello, Wo!');
-connectDatabase();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+    try {
+        await connectDatabase();
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
+});
+
+
