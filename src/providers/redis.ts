df700e91377a29b225b4d98e46482750c9718ed0
Madni <mohammedmadni09@gmail.com>
2025-05-31 00:58:13 +0530

    refactor: remove password option from Redis client configuration

--- src/providers/redis.ts
+++ src/providers/redis.ts
@@ -5,7 +5,6 @@ const redisClient = createClient({
     host: process.env.REDIS_HOST || 'localhost',
     port: Number(process.env.REDIS_PORT) || 6379,
   },
-  password: process.env.REDIS_PASSWORD,
 });
 
 redisClient.on('error', (err) => {
