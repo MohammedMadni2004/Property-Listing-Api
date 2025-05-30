83bd2dd8719b2d666810f911c3c8f4dc5bb0cf36
Madni <mohammedmadni09@gmail.com>
2025-05-31 03:51:08 +0530

    refactor: remove unused DecodedToken interface from tokenUtils

--- src/utils/tokenUtils.ts
+++ src/utils/tokenUtils.ts
@@ -2,11 +2,7 @@ import jwt, { JwtPayload } from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
-interface DecodedToken {
-    userId: string;
-    iat: number;
-    exp: number;
-}
+
 
 export const createToken = (userId: string):string => {
     return jwt.sign({userId}, process.env.JWT_SECRET || 'hello', {expiresIn: "1h"});
