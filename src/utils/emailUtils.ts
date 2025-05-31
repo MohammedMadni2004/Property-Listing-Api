0b47d436ad2160d48ccc856dff6ac11718267bf1
Madni <mohammedmadni09@gmail.com>
2025-05-31 16:01:54 +0530

    refactor: update email sending method to use HTML format and improve token expiration time to 7 days

--- src/utils/emailUtils.ts
+++ src/utils/emailUtils.ts
@@ -5,11 +5,12 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function sendEmail(to: string, subject: string, text: string) {
   try {
     await resend.emails.send({
-      from: process.env.EMAIL_FROM || "",
-      to,
-      subject,
-      text,
+      from: `Property Listing <${process.env.EMAIL_FROM || 'default@example.com'}>`,
+      to: to,
+      subject: subject,
+      html: `<p>${text}</p>`, 
     });
+    
     console.log(`Email sent to ${to}`);
   } catch (error) {
     console.error(`Error sending email to ${to}:`, error);
--- src/utils/tokenUtils.ts
+++ src/utils/tokenUtils.ts
@@ -2,10 +2,8 @@ import jwt, { JwtPayload } from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
-
-
 export const createToken = (userId: string):string => {
-    return jwt.sign({userId}, process.env.JWT_SECRET || 'hello', {expiresIn: "1h"});
+    return jwt.sign({ userId }, process.env.JWT_SECRET || 'hello', { expiresIn: '7d' });
 };
 
 export const verifyToken = (token: string) => {
